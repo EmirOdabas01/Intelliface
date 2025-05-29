2a7f738f5451d0526265e5662cec35068d33b358
Emir Odabas <odabasemir01@gmail.com>
2025-05-29 19:12:52 +0300

    location page done

--- Intelliface/Controllers/LocationController.cs
+++ Intelliface/Controllers/LocationController.cs
@@ -1,6 +1,7 @@
 ﻿using Intelliface.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 
 namespace Intelliface.Controllers
@@ -24,6 +25,56 @@ namespace Intelliface.Controllers
             var locations = JsonSerializer.Deserialize<List<ReadVM<LocationVM>>>(json);
 
             return View(locations);
+        }
+
+        public async Task<IActionResult> Create() => View();
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(LocationVM location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Redirect("Create");
+            }
+            var json = JsonSerializer.Serialize(location);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("/api/Location/Create", content);
+            response.EnsureSuccessStatusCode();
+
+            return RedirectToAction("Records");
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var response = await _httpClient.GetAsync($"/api/Location/GetById/{id}");
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            var location = JsonSerializer.Deserialize<ReadVM<LocationVM>>(json);
+            return View(location);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(int id, [FromBody] LocationVM location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Redirect("Update");
+            }
+            var json = JsonSerializer.Serialize(location);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync($"/api/Location/Update/{id}", content);
+            response.EnsureSuccessStatusCode();
+
+            return RedirectToAction("Records");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _httpClient.DeleteAsync($"/api/Location/Delete/{id}");
+            response.EnsureSuccessStatusCode();
+            return RedirectToAction("Records");
         }
     }
 }
--- Intelliface/Program.cs
+++ Intelliface/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,11 +12,20 @@ client.BaseAddress = new Uri("http://localhost:5206")
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+var supportedCultures = new[] { new CultureInfo("en-US") };
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en-US"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
+
+ 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
